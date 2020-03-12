23c4f17f52da5714c5905698e3edad177eda65ca
ven07643 <57476069+ven07643@users.noreply.github.com>
2020-03-12 12:05:10 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LBW新配信システム負荷テスト用ページ</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-left">
+          <img class="logo" src="./img/LBW_logo.png">
+        </div>
+        <span class="fa fa-bars menu-icon"></span>
+        <div class="header-right">
+          <a href="#">Life Business Weather Inc.</a>
+          <a href="#">http://www.lbw.co.jp</a>
+          <a href="#" class="login">ログイン</a>
+        </div>
+      </div>
+    </header>
+    <div class="top-wrapper">
+      <div class="container">
+        <h1>Life Business Weather</h1>
+        <h1>WR-FIL-HDW-DB1</h1>
+        <p>負荷テスト用テストページ</p>
+        <div class="btn-wrapper">
+          <a href="#" class="btn signup">サインアップ</a>
+          <p>or</p>
+          <a href="#" class="btn facebook"><span class="fa fa-facebook"></span>Facebook</a>
+          <a href="#" class="btn twitter"><span class="fa fa-twitter"></span>Twitter</a>
+        </div>
+      </div>
+    </div>
+    <div class="lesson-wrapper">
+      <div class="container">
+        <div class="heading">
+          <h2>Let you to know our corporate profile!</h2>
+        </div>
+        <div class="lessons">
+          <div class="lesson">
+            <div class="lesson-icon">
+              <img src="./img/lesson_1.png">
+              <p>Message</p>
+            </div>
+            <p class="text-contents">The biometeorology is defined as "the study of the direct and indirect effects on the living body under the physical and scientific environmental conditions of the atmosphere". I strongly wanted to provide various events arisen by this human environmental change as information to make it useful for society. I thought to make this biometeorology my life-work and started this company.  I named the company “Life & Business Weather, Inc.” combining Life Weather and Business Weather. Today we provide various weather information such as "Life Weather Information" useful for people's health and "Business Weather Information" useful for business activities, and always seek innovation.Founder & Chairma, Katsutoshi Ishikawa</p>
+          </div>
+          <div class="lesson">
+            <div class="lesson-icon">
+              <img src="./img/lesson_2.png">
+              <p>Profile</p>
+            </div>
+            <p class="text-contents">Corporate Name Life & Business Weather, Inc.<br>----------------------------<br>Abbreviation LBW<br>----------------------------<br>Founded January 16, 1998<br>Capital 210,740,000 yen<br>----------------------------<br>Representative Katsutoshi Ishikawa,<br>Charman Director Mitsuhiro Maeda, CEO<br>----------------------------<br>Number of 38 Employees (incl. 21 Certified Weather forecaster)<br>Headquarter Ichigo Ningyo-cho Bld. 2F, 1-10-14 Horidome-cho, Nihombashi Chuo-ku, Tokyo 103-0012<br>----------------------------<br>Tel 03-3668-6141<br>----------------------------<br>FAX 03-3668-6145<br>----------------------------<br>Business Category Weather Forecasting License for Forecasting Services No. 83<br>----------------------------<br>Business Description Construction Weather, Health Weather,  Retail Weather, Data Destribution, etc.<br>----------------------------<br></p>
+          </div>
+          <div class="lesson">
+            <div class="lesson-icon">
+              <img src="./img/lesson_3.png">
+              <p>History</p>
+            </div>
+            <p class="text-contents">1998 Jan Founded a health information provider   “Care Network, Inc.” Mar Participated in the advanced informa  tion society model experimental   project in Kagawa Prefecture 2001 Apr Launched Life weather Merchan  Dising (LMD) business Aug Changed name to “Life Business   Weather, Inc.” 2004 May Acquired operations from “Weather   Line Co., Ltd. 2006 Jun Business alliance with DWANGO Co.,   Ltd. 2008 Aug Launched HD-Weather, 1km mesh   high-resolution weather forecast  Launched 10min Heavy Rain Forecast 2009 Mar Launched KIYOMASA, safety informa  tion provider for Construction Field 2011 Mar KIYOMASA is registered in NETIS   (New Technology Information System)   by Ministry of Land, Infrastructure,   Transport and Tourism, ”KT-100110” 2013 Oct Launched Hazard-Eye, Weather GIS   System 2015 Mar Awarded as one of 300 contributive   Small and Medium sized Enterprice</p>
+          </div>
+          <div class="lesson">
+            <div class="lesson-icon">
+              <img src="./img/lesson_4.png">
+              <p>Philosophy</p>
+            </div>
+            <p class="text-contents">We will create value that contributes to the quality improvement of many people's lives. The weather directly and deeply affects the human body, and causes changes in behaviors and emotions. Such changes affect the economic society, and cause significant differences in business activities and profits of companies.<br>Under the situation where advanced climate change brings about abnormal weather conditions today, We, Life Business Weather, build prediction mechanisms based on weather forecast as a solution, and contribute to the improvement of corporate profit and quality of life.</p>
+          </div>
+          <div class="clear"></div>
+        </div>
+      </div>
+    </div>
+    <div class="message-wrapper">
+      <div class="container">
+        <div class="heading">
+          <h2>High Definition Weather DB Server-01 負荷テスト</h2>
+          <h3>Life Business Weather さくらVPS サーバ名：WR-FIL-HDW-DB1</h3>
+        </div>
+        <span class="btn message">負荷テストGo！</span>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <img src="./img/LBW_logo.png">
+        <p>Life Business Weather Inc.</p>
+      </div>
+    </footer>
+  </body>
+</html>
